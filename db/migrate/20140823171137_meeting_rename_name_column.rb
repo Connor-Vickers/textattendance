d96d6fe1c6bea8af071d3b81e4ecfb61c5b53d17
Connor Vickers <connorvic97@gmail.com>
2014-09-15 00:11:43 +0200

    before brower

--- db/migrate/20140823171137_meeting_rename_name_column.rb
+++ db/migrate/20140823171137_meeting_rename_name_column.rb
@@ -1,5 +1,5 @@
 class MeetingRenamenameColumn < ActiveRecord::Migration
   def change
-    rename_column :meetings, :Name, :name
+    rename_column :meetings, :Name, :name#may fail change :name to :name but couldn't remigrate
   end
 end
