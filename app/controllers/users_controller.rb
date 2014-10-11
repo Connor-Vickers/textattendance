164bcf0a922eddc029578e8da2c3874747ce0f2c
Connor Vickers <connorvic97@gmail.com>
2014-10-11 21:01:40 +0200

    changed to unicorn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
 	def create
 	  @user = User.new(user_params)
-	  if @user.email == "hvicker@purdue.edu" 
+	  if @user.email == "hvicker@purdue.edu" or  @user.email == "connorvic97@gmail.com"
       if @user.save
         session[:user_id] = @user.id
         redirect_to courses_url
