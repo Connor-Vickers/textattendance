a52146e8cd7abad590f1cda5bac73fd6aa19f324
Connor Vickers <connorvic97@gmail.com>
2014-11-02 16:44:29 +0100

    added mit login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
 	def create
 	  @user = User.new(user_params)
-	  if @user.email == "hvicker@purdue.edu" or  @user.email == "connorvic97@gmail.com"
+	  if @user.email == "hvicker@purdue.edu" or  @user.email == "connorvic97@gmail.com" or @user.email == "mit"
       if @user.save
         session[:user_id] = @user.id
         redirect_to courses_url
